9e65166d85d17358c1f3e9b3a3d79d1203d5b03a
pixelpush-io <290051917+pixelpush-io@users.noreply.github.com>
2026-08-28 23:15:21 +0100

    Disable automatic home screen icons by default

--- src/com/android/launcher3/SessionCommitReceiver.java
+++ src/com/android/launcher3/SessionCommitReceiver.java
@@ -104,6 +104,6 @@ public class SessionCommitReceiver extends BroadcastReceiver {
                 && UserCache.getInstance(context).getUserInfo(user).isPrivate()) {
             return false;
         }
-        return LauncherPrefs.getPrefs(context).getBoolean(ADD_ICON_PREFERENCE_KEY, true);
+        return LauncherPrefs.getPrefs(context).getBoolean(ADD_ICON_PREFERENCE_KEY, false);
     }
 }
